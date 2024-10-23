8f0a183a35482a1c25b401bb236e0f356d11b2da
shubham88fru <shubham88fru@yahoo.com>
2024-10-22 20:40:51 -0700

    resetting up

--- chatpdf/src/app/page.tsx
+++ chatpdf/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="text-3xl text-red-800 font-bold underline">
+      Hey There!
+      <Button>Works?</Button>
+    </h1>
+  );
 }
